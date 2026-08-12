464a4454b027f31accd4b504a03583ed913505d2
Aleksandra Trailovic <aleksandratrailovic97@gmail.com>
2026-08-12 19:05:46 +0200

    Show navigation only for authenticated users

--- vue-aplikacija/src/components/Navigacija.vue
+++ vue-aplikacija/src/components/Navigacija.vue
@@ -3,55 +3,56 @@ import { useRouter } from 'vue-router'
 
 import { useAutentifikacijaStore } from '@/stores/autentifikacija'
 
-const autentifikacijaStore = useAutentifikacijaStore()
 const router = useRouter()
+const autentifikacijaStore = useAutentifikacijaStore()
 
-function odjavi() {
+function odjaviSe() {
   autentifikacijaStore.odjavi()
   router.push({ name: 'prijava' })
 }
 </script>
 
 <template>
-  <nav class="navbar bg-dark" data-bs-theme="dark">
-    <div class="container d-flex justify-content-between">
-      <RouterLink to="/" class="navbar-brand">
-        Upravljanje zadacima
-      </RouterLink>
+  <nav
+    v-if="autentifikacijaStore.prijavljen"
+    class="navbar navbar-expand-lg navbar-dark bg-dark px-3"
+  >
+    <div class="container-fluid d-flex justify-content-between">
+      <div class="d-flex align-items-center gap-4">
+        <RouterLink
+          to="/"
+          class="navbar-brand mb-0"
+        >
+          Upravljanje zadacima
+        </RouterLink>
 
-      <div class="d-flex align-items-center gap-3">
-        <RouterLink to="/" class="nav-link text-light">
+        <RouterLink
+          to="/"
+          class="nav-link text-white"
+        >
           Početna
         </RouterLink>
 
-        <template v-if="autentifikacijaStore.prijavljen">
-          <RouterLink
-            to="/zadaci"
-            class="nav-link text-light"
-          >
-            Zadaci
-          </RouterLink>
-
-          <span class="text-light small">
-            {{ autentifikacijaStore.email }}
-          </span>
-
-          <button
-            type="button"
-            class="btn btn-outline-light btn-sm"
-            @click="odjavi"
-          >
-            Odjavi se
-          </button>
-        </template>
-
         <RouterLink
-          v-else
-          to="/prijava"
-          class="btn btn-outline-light btn-sm"
+          to="/zadaci"
+          class="nav-link text-white"
         >
-          Prijava
+          Zadaci
         </RouterLink>
+      </div>
+
+      <div class="d-flex align-items-center gap-3">
+        <span class="navbar-text text-white">
+          <strong>{{ autentifikacijaStore.email }}</strong>
+        </span>
+
+        <button
+          type="button"
+          class="btn btn-outline-light btn-sm"
+          @click="odjaviSe"
+        >
+          Odjavi se
+        </button>
       </div>
     </div>
   </nav>
